c514876fa61edb042b88d327e8f578d5ad612455
Sweedsiter <d27saitunlu@gmail.com>
2023-07-28 16:53:42 +0700

    Home-edit001

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Home.css'
 
 function Home({Search}) {
+  const [PrValee,setPrValue] = useState("")
+  
   return (
-    <div>
-        <h1>Home Page</h1>
+    <div className='H-contener'>
+       <div className='L'>
+          <h3>Your search?</h3>
+          <div>Search  : :  
+            <input type='text'/>
+          </div>
+          <div>
+            Start Price
+            <input type='range'  min="1000" max="1000000"onChange={(e)=>setPrValue(e.target.value)} />             
+            <div>Price : {PrValee}</div>
+          </div>         
+       </div>
+
+       <div className='R'>
+        <h1>Items Card</h1>
         <h1>{Search}</h1>
+        <p>{PrValee}</p>
+       </div>
     </div>
   )
 }
