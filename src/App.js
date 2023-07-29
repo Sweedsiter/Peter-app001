f7c0213d336d1045a8710162e96b8db83c812ea9
Sweedsiter <d27saitunlu@gmail.com>
2023-07-29 14:57:00 +0700

    home-edit3

--- src/App.js
+++ src/App.js
@@ -7,25 +7,32 @@ import { Routes, Route} from "react-router-dom"
 import Home from "./Components/Home/Home";
 import About from "./Components/About/About";
 import Conext from "./Components/Conext/Conext";
+import styled from 'styled-components'
+import './App.css'
 
 function App() { 
   const [Search,setSearch ] = useState() 
   const [Ncolor,setNcolor ] = useState() 
-  const Color = (e)=>{
-    setNcolor(e)
-  }
+const Div = styled.div({
+  color:`${Ncolor}` ,
+  height:`100vh`,
+});
+
 
   return (
-    <div style={{color:`${Ncolor}`}}>
+    <Div>
+    
       <UserContext.Provider value={Logo} className="App"> 
-         <Nav search={(e)=>setSearch(e)}/>   
+         <Nav search={(e)=>setSearch(e)}/> 
+        
          <Routes>
-            <Route path="/" exec element={ <Home Search={Search} Color={Color}/> } />          
+            <Route path="/" exec element={ <Home search={Search} Color={(e)=>setNcolor(e)} /> } />          
             <Route path="/about"  element={ <About /> } />          
             <Route path="/conext"  element={ <Conext /> } />          
          </Routes>  
-       </UserContext.Provider>
-    </div>
+       </UserContext.Provider>  
+       
+    </Div>
   );
 }
 
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,45 +1,42 @@
 import React, { useState } from 'react'
 import './Home.css'
+import styled from 'styled-components'
 
-function Home({Search,Color}) {
-  const [PrValee,setPrValue] = useState("")
+function Home({Color,}) {
+  //Colors
+  const [PrValue,setPrValue] = useState()
   const [PDcolor,setPDcolor] = useState("")
-
-  
+  const [Search,setSearch] = useState()
+  const Div = styled.section({
+    background: PDcolor,   
+  });
+ 
   return (
-    <div className='H-contener' >
-      
-       <div className='L' style={{}}>
-          <div>
-             Font color  <input type="color" onChange={(e)=>Color(e.target.value)}/>
-          </div> 
-
-          <h3>Your search?</h3>
-          <div>Search  : :  
-            <input type='text' value={PrValee} />
+    <div className='H-contener' style={{background: PDcolor,}} >      
+       <div className='L setFlex-C' >
+          <div className='setColor border-under'>
+              <h3> What You need Color in App?</h3>
+              <div className='setFlex-R'>Font color  <input type="color" onChange={(e)=>Color(e.target.value)}/></div> 
+              <div className='setFlex-R'>background-Color<input type="color" onChange={(e)=>setPDcolor(e.target.value)} /></div>                          
           </div>
-          <div>
-            Start Price
-            <input type='range'  min="1000" max="1000000"onChange={(e)=>setPrValue(e.target.value)} /> 
-          </div> 
-
-          <div>          
-            <div style={{color:"red"}}> Red
-               <input type="checkbox" value="red" exact onChange={(e)=>setPDcolor(e.target.value)} /> 
+          <div className='border-under'>
+             <h3>Your search Price ?</h3>
+             <div className='setFlex-R'>
+                 <input type='range' min="0" max="10000"  onChange={(e)=>setPrValue(e.target.value)} />
+                 <p>{PrValue}</p>
              </div>
-            <div style={{color:"green"}}>
-            Green
-               <input type="checkbox" value="Green" exact onChange={(e)=>setPDcolor(e.target.value)} /> 
-            </div>
-          </div>   
-             <h2>{PDcolor}</h2>
+          </div> 
+          <div className='border-under'>  
+              <p>Search Text</p>
+              <input type="text"  placeholder="Search" onChange={(e)=>setSearch(e.target.value)}/>
+          </div>  
        </div>
 
-       <div className='R'>
+       <Div className='R'>
         <h1>Items Card</h1>
         <h1>{Search}</h1>
-        <p>{PrValee}</p>
-       </div>
+        <p>{PrValue}</p>
+       </Div>
     </div>
   )
 }
